cd2627ca92ed121ae30a59d40ccbe52ac4e78377
MatheusZamo <19matheushenrique03@gmail.com>
2024-09-24 19:02:46 -0300

    Logo component and initial list created

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,36 @@
-const App = () => <h1>Boilerplate Vite + React</h1>
+import { Logo } from "./components/logo"
+
+const App = () => {
+  return (
+    <>
+      <Logo />
+      <div className="app">
+        <div className="sidebar">
+          <ul>
+            <li>
+              <img src="henrique-48.jpg" alt="" />
+              <h3>Henrique</h3>
+              <p className="red-debit">Você deve 7 reais</p>
+              <button className="button">Selecionar</button>
+            </li>
+            <li>
+              <img src="renata-48.jpg" alt="" />
+              <h3>Renata</h3>
+              <p className="green-credit">Te deve 20 reais</p>
+              <button className="button">Selecionar</button>
+            </li>
+            <li>
+              <img src="antonio-48.jpg" alt="" />
+              <h3>Antônio</h3>
+              <p>Estão quites</p>
+              <button className="button">Selecionar</button>
+            </li>
+          </ul>
+          <button className="button">Adicionar</button>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export { App }
---
+++ src/components/logo.jsx
@@ -0,0 +1,7 @@
+const Logo = () => (
+  <header className="header">
+    <img src="logo-racha-conta.png" alt="Logo do racha conta" />
+  </header>
+)
+
+export { Logo }
