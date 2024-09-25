10d6ceaab5790e30d1a1e3ab1b2aca05fe5b64ba
MatheusZamo <19matheushenrique03@gmail.com>
2024-09-25 11:23:46 -0300

    update in the rendering of the friends list to a dynamic form with the map

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,47 @@
 import { Logo } from "./components/logo"
 
 const App = () => {
+  const friends = [
+    {
+      id: crypto.randomUUID(),
+      img: "henrique-48.jpg",
+      name: "Henrique",
+      classs: "red-debit",
+      message: "Você deve 7 reais",
+    },
+    {
+      id: crypto.randomUUID(),
+      img: "renata-48.jpg",
+      name: "Renata",
+      classs: "green-credit",
+      message: "Te deve 20 reais",
+    },
+    {
+      id: crypto.randomUUID(),
+      img: "antonio-48.jpg",
+      name: "Antônio",
+      classs: "white-neutral",
+      message: "Estão quites",
+    },
+  ]
   return (
     <>
       <Logo />
       <div className="app">
         <div className="sidebar">
           <ul>
-            <li>
-              <img src="henrique-48.jpg" alt="" />
-              <h3>Henrique</h3>
-              <p className="red-debit">Você deve 7 reais</p>
-              <button className="button">Selecionar</button>
-            </li>
-            <li>
-              <img src="renata-48.jpg" alt="" />
-              <h3>Renata</h3>
-              <p className="green-credit">Te deve 20 reais</p>
-              <button className="button">Selecionar</button>
-            </li>
-            <li>
-              <img src="antonio-48.jpg" alt="" />
-              <h3>Antônio</h3>
-              <p>Estão quites</p>
-              <button className="button">Selecionar</button>
-            </li>
+            {friends.map(({ id, img, name, classs, message }) => (
+              <li key={id}>
+                <img src={img} alt="" />
+                <h3>{name}</h3>
+                <p className={classs}>{message}</p>
+                <button className="button">Selecionar</button>
+              </li>
+            ))}
           </ul>
           <button className="button">Adicionar</button>
         </div>
+
         <form className="form-add-friend form-split-bill">
           <h2>Rache a conta com Ântonio</h2>
           <label>
