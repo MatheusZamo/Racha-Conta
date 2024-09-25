f8918ab2707177b5884bca1704e3286595bae3ec
MatheusZamo <19matheushenrique03@gmail.com>
2024-09-25 11:58:07 -0300

    showing message and adding class dynamically

--- src/app.jsx
+++ src/app.jsx
@@ -1,40 +1,55 @@
+import { useState } from "react"
 import { Logo } from "./components/logo"
 
+const friends = [
+  {
+    id: crypto.randomUUID(),
+    img: "henrique-48.jpg",
+    name: "Henrique",
+    value: -7,
+  },
+  {
+    id: crypto.randomUUID(),
+    img: "renata-48.jpg",
+    name: "Renata",
+    value: 20,
+  },
+  {
+    id: crypto.randomUUID(),
+    img: "antonio-48.jpg",
+    name: "Antônio",
+    value: 0,
+  },
+]
+
 const App = () => {
-  const friends = [
-    {
-      id: crypto.randomUUID(),
-      img: "henrique-48.jpg",
-      name: "Henrique",
-      classs: "red-debit",
-      message: "Você deve 7 reais",
-    },
-    {
-      id: crypto.randomUUID(),
-      img: "renata-48.jpg",
-      name: "Renata",
-      classs: "green-credit",
-      message: "Te deve 20 reais",
-    },
-    {
-      id: crypto.randomUUID(),
-      img: "antonio-48.jpg",
-      name: "Antônio",
-      classs: "white-neutral",
-      message: "Estão quites",
-    },
-  ]
+  const [listFriends, setListFriends] = useState(friends)
+
   return (
     <>
       <Logo />
       <div className="app">
         <div className="sidebar">
           <ul>
-            {friends.map(({ id, img, name, classs, message }) => (
+            {listFriends.map(({ id, img, name, value }) => (
               <li key={id}>
                 <img src={img} alt="" />
                 <h3>{name}</h3>
-                <p className={classs}>{message}</p>
+                <p
+                  className={
+                    value === 0
+                      ? "white-neutral"
+                      : value < 0
+                      ? "red-debit"
+                      : "green-credit"
+                  }
+                >
+                  {value === 0
+                    ? "Estão quites"
+                    : value < 0
+                    ? `Você deve ${Math.abs(value)} reais`
+                    : `Te deve ${value} reais`}
+                </p>
                 <button className="button">Selecionar</button>
               </li>
             ))}
